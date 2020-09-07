b66a2e886f7f456110e0ef4b817b6454bade4eaf
JimmyMcBride <mcbride967@gmail.com>
2020-09-07 01:50:34 -0500

    new changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-export { default as EsoSetItem } from "./Tooltip";
-export { default as EsoGearTable } from "./Table";
+// export { default as EsoSetItem } from "./Tooltip";
+// export { default as EsoGearTable } from "./Table";
+import EsoSetItem from "./Tooltip";
+import EsoGearTable from "./Table";
