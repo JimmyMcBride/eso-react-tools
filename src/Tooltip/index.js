2b3dc2f54189b463993e4daff53ba43d90186d65
JimmyMcBride <mcbride967@gmail.com>
2020-09-07 02:50:16 -0500

    new changes

--- src/Tooltip/index.js
+++ src/Tooltip/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Tooltip, Card, Text, Box, theme } from "sriracha-ui";
 // import { Tooltip, Text, theme } from "sriracha-ui";
-import Error from "./Error";
-import Loading from "./Loading";
-import axios from "axios";
+// import Error from "./Error";
+// import Loading from "./Loading";
+// import axios from "axios";
 // import "sriracha-ui/css/main.css";
 import "./styles.css";
 
 const EsoSetItem = ({ name, ...rest }) => {
-  const [set, setSet] = React.useState(null);
-  const [error, setError] = React.useState(null);
+  // const [set, setSet] = React.useState(null);
+  // const [error, setError] = React.useState(null);
 
-  React.useEffect(() => {
-    axios
-      .get(`https://eso-sets.herokuapp.com/set/${name}`)
-      .then((res) => setSet(res.data))
-      .catch((err) => setError(err.message));
-  }, [name]);
+  // React.useEffect(() => {
+  //   axios
+  //     .get(`https://eso-sets.herokuapp.com/set/${name}`)
+  //     .then((res) => setSet(res.data))
+  //     .catch((err) => setError(err.message));
+  // }, [name]);
 
   return (
     <Tooltip {...rest}>
