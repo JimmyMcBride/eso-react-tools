3245f55a4a74df8929ba38224d447ac5cc2f1397
JimmyMcBride <mcbride967@gmail.com>
2020-09-06 23:07:30 -0500

    new changes

--- src/Table/index.js
+++ src/Table/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, theme } from "sriracha-ui";
-import { EsoSetItem } from "../Tooltip";
+import EsoSetItem from "../Tooltip";
 import "./styles.css";
 
 export default function EsoGearTable({ data }) {
