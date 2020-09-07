bfb75001cb5f95228acdcbd80b79de92d28e7f7f
JimmyMcBride <mcbride967@gmail.com>
2020-09-07 02:19:03 -0500

    new changes

--- src/Table/index.js
+++ src/Table/index.js
@@ -6,9 +6,9 @@ import "./styles.css";
 export default function EsoGearTable({ data }) {
   return (
     <Card invert bg={theme.colors.gray9} p="1rem" radius="0.3rem">
-      <table style={{ width: "550px" }}>
+      <table className="gear-table">
         <thead>
-          <tr style={{ width: "100%", background: theme.colors.amber8 }}>
+          <tr className="head-row">
             <th>Gear</th>
             <th>Set</th>
             <th>Weight</th>
@@ -17,14 +17,7 @@ export default function EsoGearTable({ data }) {
           </tr>
           {data.map((item) => {
             return (
-              <tr
-                key={item.id}
-                style={{
-                  width: "100%",
-                  background: theme.colors.whiteAlpha2,
-                  borderRadius: "0.5rem",
-                }}
-              >
+              <tr key={item.id} className="data-row">
                 <td>{item.gear}</td>
                 <td>
                   <EsoSetItem name={item.set} ttM="-2rem 0 0 14.5rem" />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // export { default as EsoSetItem } from "./Tooltip";
 // export { default as EsoGearTable } from "./Table";
 import EsoSetItem from "./Tooltip";
-// import EsoGearTable from "./Table";
+import EsoGearTable from "./Table";
 
-export { EsoSetItem };
+export { EsoSetItem, EsoGearTable };
